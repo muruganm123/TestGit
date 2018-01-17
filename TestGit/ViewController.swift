fffcd5b2edd16bf2d7e96315aa6fb55e9c953e1c
Murugan <Murugan0461@gmail.com>
2018-01-17 16:26:21 +0530

    dummy commit

--- TestGit/ViewController.swift
+++ TestGit/ViewController.swift
@@ -14,7 +14,6 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         print("Hello")
         print("World")
-        print("testkit 2.0")//adde comment  //new changes
         // Do any additional setup after loading the view, typically from a nib.
     }
 
